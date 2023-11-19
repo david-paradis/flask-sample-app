602b59b29a5ac9da8fef996a22fd445291d0b838
David Paradis <davidparadis@MacBook-Air-de-David.local>
2023-11-19 12:03:52 -0500

    Added link for back to home

--- src/app.py
+++ src/app.py
@@ -11,10 +11,13 @@ def main():
      <form action="/echo_user_input" method="POST">
          <input name="user_input">
          <input type="submit" value="Submit!">
-     </form>
+     </form>    
      '''
 
 @app.route("/echo_user_input", methods=["POST"])
 def echo_input():
     input_text = request.form.get("user_input", "")
-    return "Welcome " + input_text + ". Nice to meet you!"
+    return "Welcome " + input_text + ". Nice to meet you!" + \
+    '''
+        <div></div><a href="/">Back to home</a>
+    '''
